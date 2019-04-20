404895f4ae749b54d98a150f25e0ee1d6d76e1e9
David Malary <davidmalary@Davids-MacBook-Pro.local>
2019-04-20 14:30:08 -0400

    clean page loop

--- scrape.js
+++ scrape.js
@@ -10,119 +10,60 @@ var mangaIndex = [];
 
 let i = 1;
 for (i; i < 64; i++) {
-const options = {
-// var options = {
-  uri:'https://myanimelist.net/manga/genre/1/Action',
-  qs: {
-    page: i
-  },
-  transform: function (body) {
-    return cheerio.load(body);
-  }
-};
-
-rp(options)
-  .then(($) => {
-    // TEST
-    // console.log($);
-
-    // jsonframe SETUP
-    jsonframe($)
-
-    var frame = {
-      "manga": {
-        _s: ".seasonal-anime",
-        _d: [{
-          "title": ".link-title < html",
-          "url": "a.link-title @ href",
-          // "url": ".title-text a @ href",
-          "author": ".producer",
-          "imageUrl": "img.image @ src",
-          "score": ".score < html"
-        }]
-      }
-    };
-
-    // push to array?
-    var mangaList = $('body').scrape(frame);
-    // console.log(mangaList);
-    mangaIndex.push(mangaList);
-
-    // // put 2nd request in a for loop with URL + i, and new cheerio.load(body2)?
-    // let i = 1;
-    // for (i; i < 64; i++) {
-    //   const options2 = {
-    //     uri:'https://myanimelist.net/manga/genre/1/Action',
-    //     qs: {
-    //       page: i
-    //     },
-    //     transform: function (body2) {
-    //       return cheerio.load(body2);
-    //     }
-    //   };
-    //
-    //   rp(options2)
-    //   .then(($) => {
-    //
-    //     // TEST
-    //     // console.log($);
-    //
-    //     // jsonframe SETUP
-    //     jsonframe($)
-    //
-    //     var frame = {
-    //       "manga": {
-    //         _s: ".seasonal-anime",
-    //         _d: [{
-    //           "title": ".link-title < html",
-    //           "url": "a.link-title @ href",
-    //           // "url": ".title-text a @ href",
-    //           "author": ".producer",
-    //           "imageUrl": "img.image @ src",
-    //           "score": ".score < html"
-    //         }]
-    //       }
-    //     };
-    //
-    //     var mangaList2 = $('body').scrape(frame);
-    //     // console.log(mangaList2);
-    //     mangaIndex.push(mangaList2);
-    //     // console.log(mangaIndex[5]);
-    //
-    //
-    //     if (i == 64 ) {
-    //       function writeFile() {
-    //         fs.writeFile("data.json", JSON.stringify(mangaIndex, null, 4), (err) => {
-    //           console.log(
-    //             '====================================' + '\n' +
-    //             'File write success!' + '\n' +
-    //             'Data file located in data.json ' +
-    //             '(' + mangaIndex.length + ' items)' + '\n' + '====================================' );
-    //         });
-    //       }
-    //
-    //       writeFile();
-    //     }
-    //   })
-    // }
-
-    // writeFile
-    // add if after loop then writefile?
-        if (i == 64 ) {
-    function writeFile() {
-      fs.writeFile("data.json", JSON.stringify(mangaIndex, null, 4), (err) => {
-        console.log(
-          '====================================' + '\n' +
-          'File write success!' + '\n' +
-          'Data file located in data.json ' +
-          '(' + mangaIndex.length + ' items)' + '\n' + '====================================' );
-      });
+  const options = {
+    uri: 'https://myanimelist.net/manga/genre/1/Action',
+    qs: {
+      page: i
+    },
+    transform: function(body) {
+      return cheerio.load(body);
     }
-  }
+  };
 
-    writeFile();
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+  rp(options)
+    .then(($) => {
+      // TEST
+      // console.log($);
+
+      // jsonframe SETUP
+      jsonframe($)
+
+      var frame = {
+        "manga": {
+          _s: ".seasonal-anime",
+          _d: [{
+            "title": ".link-title < html",
+            "url": "a.link-title @ href",
+            // "url": ".title-text a @ href",
+            "author": ".producer",
+            "imageUrl": "img.image @ src",
+            "score": ".score < html"
+          }]
+        }
+      };
+
+      // PUSH TO DATA ARRAY
+      var mangaList = $('body').scrape(frame);
+      // console.log(mangaList);
+      mangaIndex.push(mangaList);
+
+      // writeFile FUNCTION
+      if (i == 64) {
+        function writeFile() {
+          fs.writeFile("data.json", JSON.stringify(mangaIndex, null, 4), (err) => {
+            console.log(
+              '====================================' + '\n' +
+              'File write success!' + '\n' +
+              'Data file located in data.json ' +
+              '(' + mangaIndex.length + ' items)' + '\n' +
+              '====================================');
+          });
+        }
+      }
+
+      writeFile();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 }
